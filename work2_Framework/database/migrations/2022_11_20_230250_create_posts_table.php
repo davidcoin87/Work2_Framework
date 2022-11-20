7894821237cae81d6402318fc41120975e5b5982
David Ruiz <davidcoin87@gmail.com>
2022-11-20 18:19:43 -0500

    Pasos iniciales
    Creción de migraciones, modelos y controladores

---
+++ work2_Framework/database/migrations/2022_11_20_230250_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')
+            ->constrained('categories')
+            ->cascadeOnDelete()
+            ->cascadeOnUpdate();
+            $table->string('name');
+            $table->string('desription')->nullable();
+            $table->tinyInteger('state')->comment('0: No publicado, 1: Publicado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
